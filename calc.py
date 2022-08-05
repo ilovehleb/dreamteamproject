8a4f4f2d325653bb791965805534cdd5570a1d07
ilovehleb <108723953+ilovehleb@users.noreply.github.com>
2022-08-05 10:45:33 +0300

    Добавил степень и корень

--- calc.py
+++ calc.py
@@ -1,48 +1,76 @@
 #Calc
+
+
 while True:
     print("\n"
         "Ввод 'Q' останавливает программу."
           "\n")
+
+
     a = input('Введите первое число:  ')
+    if a=="q" or a=="Q":
+        break
     if not a.isdigit():
         print("\n"
               'Не является числом!'
-              "\n")
+              "\n____________________")
         continue
-    if a=="q" or a=="Q":
-        break
-    x = input('Знак действия:  ')
-    if x!='+' and x!='-' and x!='*' and x!='/':
+
+
+    x = input('Знак действия(+,-,*,/,**,sqrt):  ')
+    if x!='+' and x!='-' and x!='*' and x!='/' and x!='**' and x!='sqrt':
         print("\n"
               "Не верный знак."
-              "\n")
+              "\n________________")
         continue
     if x=="q" or x=="Q":
         break
+
+
     b = input('Введите второе число:  ')
+    if b=="q" or b=="Q":
+        break
     if not b.isdigit():
         print("\n"
               'Не является числом!'
-              "\n")
+              "\n____________________")
         continue
-    if b=="q" or b=="Q":
-        break
-    if a=="q" or a=="Q" or x=="q" or x=="Q" or b=="q" or b=="Q":
-        break
-    elif x=="+":
+
+
+    if x=="+":
         print("\n"
-              "Ответ:  ",int(a)+int(b))
+              "Ответ:  ",int(a)+int(b)
+              "\n____________________")
+
+
     elif x=="-":
         print("\n"
-              "Ответ:  ",int(a)-int(b))
+              "Ответ:  ",int(a)-int(b)
+              "\n____________________")
+
+
     elif x=="*":
         print("\n"
-              "Ответ:  ",int(a)*int(b))
+              "Ответ:  ",int(a)*int(b)
+              "\n____________________")
+
+
+    elif x=='**':
+        print("\n"
+              "Ответ:  ",int(a)**int(b)
+              "\n____________________")
+
+
+    elif x=='sqrt':
+        print("\n"
+              "Ответ:  ",int(b)**(1/int(a))
+              "\n____________________")
     else:
         if b=='0':
             print("Делить на ноль нельзя!")
         else:
             print("\n"
-                  "Ответ:  ",int(a)/int(b))
+                  "Ответ:  ",int(a)/int(b)
+                  "\n____________________")
 
 
